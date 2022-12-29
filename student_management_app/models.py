e77d6856dbec7b478c7a372a76c86efe8ecd4410
Ololade0 <adesuyiololade@gmail.com>
2022-12-29 17:36:18 +0100

    college_management_system

---
+++ student_management_app/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    HOD = '1'
+    STAFF = '2'
+    STUDENT = '3'
+     
+    EMAIL_TO_USER_TYPE_MAP = {
+        'hod': HOD,
+        'staff': STAFF,
+        'student': STUDENT
+    }
+ 
+    user_type_data = ((HOD, "HOD"), (STAFF, "Staff"), (STUDENT, "Student"))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+ 
+ 
+
+# Create your models here.
